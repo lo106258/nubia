14ddd99e2c72c47504e231e25af89b5afc188758
QLOPC\QloPC-zs <lo106258@qq.com>
2021-12-31 10:20:24 +0800

    #可用v2ray版本

--- Backstage.sh
+++ Backstage.sh
@@ -7,140 +7,100 @@ YELLOW="\033[33m"
 BLUE="\033[36m"
 BLANK="\033[0m"
 
-colorEcho() {
+colorEcho(){
     COLOR=$1
-    shift # delete first parameter
-    echo -e "${COLOR}${@}${BLANK}"
+    echo -e "${COLOR}${@:2}${BLANK}"
     echo
 }
 
-colorRead() {
+colorRead(){
     COLOR=$1
     OUTPUT=$2
     VARIABLE=$3
     echo -e -n "$COLOR$OUTPUT${BLANK}: "
-    read $VARIABLE </dev/tty
+    read $VARIABLE
     echo
 }
 
-cmd_need() {
-    update_flag=0
-    exit_flag=0
-
-    for cmd in $1; do
-        if ! command -v $cmd >/dev/null 2>&1; then
-            # check if auto install
-            if command apt >/dev/null 2>&1; then
-                # apt install package need update first
-                if [ "update_flag" = "0" ]; then
-                    apt update >/dev/null 2>&1
-                    update_flag=1
-                fi
-
-                package=$(dpkg -S bin/$cmd 2>&1 | grep "bin/$cmd$" | awk -F':' '{print $1}')
-                if [ ! -z "$package" ]; then
-                    colorEcho $BLUE "正在安装 $cmd ..."
-                    apt install $package -y >/dev/null 2>&1
-                    continue
-                fi
-            elif command yum >/dev/null 2>&1; then
-                package=$(yum whatprovides *bin/$cmd 2>&1 | grep " : " | awk -F' : ' '{print $1}' | sed -n '1p')
-                if [ ! -z "$package" ]; then
-                    colorEcho $BLUE "正在安装 $cmd ..."
-                    yum install $package -y >/dev/null 2>&1
-                    continue
-                fi
-            fi
-
-            colorEcho $RED "找不到 $cmd 命令"
-            exit_flag=1
-        fi
+cmd_need(){
+    [ -z "$(command -v yum)" ] && CHECK=$(dpkg -l) || CHECK=$(rpm -qa)
+    for command in $1;do
+        echo "$CHECK" | grep -q "$command" || CMD="$command $CMD"
     done
-
-    if [ "$exit_flag" = "1" ]; then
-        exit 1
-    fi
+    if [ ! -z "$CMD" ];then
+		colorEcho $BLUE "正在安装 $CMD ..."
+		if [ -z "$(command -v yum)" ];then
+			apt-get update
+			apt-get install $CMD -y
+		else
+			yum install $CMD -y
+		fi > /dev/null 2>&1
+		clear
+	fi
 }
 
-install_zip() {
+install_zip(){
     key="$1"
-    if [ -d "/usr" -a ! -z "$(command -v apt-get yum)" ]; then
-        wp="/usr/local/$key"
-    else
-        colorRead $YELLOW "请输入安装目录，例如：/usr/local，不能是/bin " wp
-        [ -z "$wp" ] && exit 1
-        wp=$(echo "$wp/$key" | sed 's|///*|/|g') # 连续的 / 变为一个
-    fi
+    wp="/usr/local/$key"
     zip="$key.zip"
-    if [ -d "$wp" ]; then
-        colorEcho $YELLOW "正在卸载 $key ..."
-        bash $wp/uninstall.sh >/dev/null 2>&1
-    fi
-    colorEcho $YELLOW "正在安装 $key 到 $wp ..."
-    curl -OL https://raw.githubusercontent.com/FH0/nubia/master/server_script/$zip
-    rm -rf $wp
-    mkdir -p $wp
-    unzip -q -o $zip -d $wp
-    rm -f $zip
-    sed -i "s|wp=.*|wp=\"$wp\"|g" $wp/*.sh # 修改路径
+    if [ -d "$wp" ];then
+		colorEcho $YELLOW "正在卸载 $key..." 
+		bash $wp/uninstall.sh >/dev/null 2>&1
+	fi
+    colorEcho $YELLOW "正在安装 $key 到 $wp..." 
+    curl -sOL https://raw.githubusercontent.com/lo106258/nubia/v2ray.zip/server_script/$zip
+    rm -rf $wp ; mkdir -p $wp
+    unzip -q -o $zip -d $wp ; rm -f $zip
     bash $wp/install.sh
 }
 
-check_environment() {
-    if [ "${EUID:-$(id -u)}" != "0" ]; then
-        colorEcho $RED "请切换到 root 用户后再执行此脚本！"
+check_system() {
+    clear
+    if [ -z "$(command -v yum apt-get)" ];then
+        colorEcho $RED "不支持的操作系统！"
         exit 1
-    fi
-
-    if [ "$(uname -r | awk -F '.' '{print $1}')" -lt "3" ]; then
-        colorEcho $RED "内核太老，请升级内核或更新系统！"
+    elif ! uname -m | grep -q 'x86_64';then
+        colorEcho $RED "不支持的系统架构！"
         exit 1
     fi
 }
 
-jzdh_add() {
-    JZDH_ZIP="$JZDH_ZIP$1 $2\n"
+jzdh_add(){
+	JZDH_ZIP+="$1 $2\n"
 }
 
 panel() {
-    clear
-
-    check_environment
-    cmd_need 'iptables unzip netstat curl'
-
-    jzdh_add "xray" "xray"
-    jzdh_add "BBR" "BBR"
-    jzdh_add "AriaNG" "AriaNG"
-    jzdh_add "frp" "frps"
-    jzdh_add "swap 分区" "swapfile"
-    jzdh_add "oneindex" "oneindex"
-    jzdh_add "openvpn" "openvpn"
-    jzdh_add "wireguard" "wireguard"
-    jzdh_add "tinyvpn-udp2raw" "tinyvpn"
-    jzdh_add "smartdns" "smartdns"
-    jzdh_add "tun2socks-xray 透明代理" "tun2socks"
-    jzdh_add "xray 透明代理（TPROXY + REDIRECT）" "xrayT"
-    jzdh_add "ygk" "ygk"
-    jzdh_add "l_ygk（linux 客户端）" "l_ygk"
-    jzdh_add "stn" "stn"
-    jzdh_add "l-stn（linux 客户端）" "l-stn"
-    jzdh_add "shadowsocks-rust" "shadowsocks-rust"
-    jzdh_add "qbittorrent" "qbittorrent"
-
+    check_system
+    cmd_need 'wget iproute unzip net-tools curl'
+	
+	jzdh_add "V2Ray"                    "v2ray"
+	jzdh_add "ssr_jzdh"                 "ssr_jzdh"
+	jzdh_add "BBR"                      "BBR"
+	jzdh_add "AriaNG"                   "AriaNG"
+	jzdh_add "frp"                      "frps"
+	jzdh_add "swap 分区"                 "ssr_jzdh"
+	jzdh_add "oneindex"                 "oneindex"
+	jzdh_add "openvpn"                  "openvpn"
+	jzdh_add "wireguard"                "wireguard"
+	jzdh_add "tinyvpn-udp2raw"          "tinyvpn"
+	jzdh_add "smartdns"                 "smartdns"
+	jzdh_add "tun2socks-v2ray 透明代理"  "tun2socks"
+	jzdh_add "v2ray 透明代理（TPROXY + REDIRECT）"   "v2rayT"
+	
     colorEcho $BLUE "欢迎使用 JZDH 集合脚本"
     var=1
-    echo -e "$JZDH_ZIP" | grep -Ev '^$' | while read zip; do
-        zip_path="$(echo "$zip" | awk '{print $NF}')"
-        zip_name="$(echo "$zip" | awk '{$NF=""; print $0}')"
-        if [ -d "/usr/local/$zip_path" ]; then
-            printf "%3s. 安装 ${GREEN}$zip_name${BLANK}\n" "$((var++))"
-        else
-            printf "%3s. 安装 $zip_name\n" "$((var++))"
-        fi
-    done
+	echo -e "$JZDH_ZIP" | grep -Ev '^$' | while read zip;do
+		zip_path="$(echo "$zip" | awk '{print $NF}')"
+		zip_name="$(echo "$zip" | awk '{$NF=""; print $0}')"
+		if [ -d "/usr/local/$zip_path" ];then
+			printf "%3s. 安装 ${GREEN}$zip_name${BLANK}\n" "$((var++))"
+		else
+			printf "%3s. 安装 $zip_name\n" "$((var++))"
+		fi
+	done
     echo && colorRead ${YELLOW} '请选择' panel_choice
-    [ -z "$panel_choice" ] && clear && exit 0
-    for J in $panel_choice; do
+	[ -z "$panel_choice" ] && clear && exit 0
+    for J in $panel_choice;do
         install_zip $(echo -e "$JZDH_ZIP" | sed -n "${J}p" | awk '{print $NF}')
     done
 }
